6acad558229da46fcb78f5568ed7ede06b64cb10
FrancisOfosu105 <francisofosu105@gmail.com>
2018-07-12 16:53:37 +0000

    Initial Commit

---
+++ SupCommerce.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SupCommerce.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
